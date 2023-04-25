50900c26d15603ff6926ad2c45090e9efe4b3942
Ravinder Gupta <ragu@microsoft.com>
2023-04-25 16:26:44 +0530

    test

--- Platform/Layers/Core/main.tf
+++ Platform/Layers/Core/main.tf
@@ -28,7 +28,6 @@ module "enterprise_scale" {
   root_parent_id = data.azurerm_client_config.core.tenant_id
   root_id        = var.root_id
   root_name      = var.root_name
-  library_path   = "./lib"
 
   ###### Deploys MG structure with naming convention provided by customer and disables deployment of default core structure ######
 
