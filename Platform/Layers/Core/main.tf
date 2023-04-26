121655e19a978cd62a5cfaa57b0b0778b8016551
Ravinder Gupta <ragu@microsoft.com>
2023-04-26 11:19:53 +0530

    test

--- Platform/Layers/Core/main.tf
+++ Platform/Layers/Core/main.tf
@@ -38,63 +38,33 @@ module "enterprise_scale" {
         display_name               = "${lower(var.root_name)}"
         parent_management_group_id = "${data.azurerm_client_config.core.tenant_id}"
         subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "es_root"
-          parameters     = {}
-          access_control = {}
-        }
        } 
       "${var.root_id}-decommissioned" = {
         display_name               = "${lower(var.root_id)}-decommissioned"
         parent_management_group_id = "${var.root_id}"
         subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "es_decommissioned"
-          parameters     = {}
-          access_control = {}
-        }
       }
       "${var.root_id}-platform" = {
         display_name               = "${lower(var.root_id)}-platform"
         parent_management_group_id = "${var.root_id}"
         subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "es_platform"
-          parameters     = {}
-          access_control = {}
-        }
       }
       "${var.root_id}-connectivity" = {
         display_name               = "${lower(var.root_id)}-connectivity"
         parent_management_group_id = "${var.root_id}-platform"
         subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "es_connectivity"
-          parameters     = {}
-          access_control = {}
-        }
         # depends_on= [azurerm_subscription.connectivity1,azurerm_subscription.connectivity2]
       }
       "${var.root_id}-management" = {
         display_name               = "${lower(var.root_id)}-management"
         parent_management_group_id = "${var.root_id}-platform"
         subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "es_management"
-          parameters     = {}
-          access_control = {}
-        }
         # depends_on= [azurerm_subscription.management1]
       }
       "${var.root_id}-identity" = {
         display_name               = "${lower(var.root_id)}-identity"
         parent_management_group_id = "${var.root_id}-platform"
         subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "es_identity"
-          parameters     = {}
-          access_control = {}
-        }
         # depends_on= [azurerm_subscription.identity1]
       }
       ##### landing zone #####
@@ -102,53 +72,27 @@ module "enterprise_scale" {
         display_name               = "${lower(var.root_id)}-landing-zones"
         parent_management_group_id = "${var.root_id}"
         subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "es_landing_zones"
-          parameters     = {}
-          access_control = {}
-        }
       }
       "${var.root_id}-prod" = {
         display_name               = "${lower(var.root_id)}-prod"
         parent_management_group_id = "${var.root_id}-landing-zones"
         subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "default_empty"
-          parameters     = {}
-          access_control = {}
-        }
       }
       "${var.root_id}-nonprod" = {
         display_name               = "${lower(var.root_id)}-nonprod"
         parent_management_group_id = "${var.root_id}-landing-zones"
         subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "default_empty"
-          parameters     = {}
-          access_control = {}
-        }
       }
       "${var.root_id}-dev" = {
         display_name               = "${lower(var.root_id)}-dev"
         parent_management_group_id = "${var.root_id}-landing-zones"
         subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "default_empty"
-          parameters     = {}
-          access_control = {}
-        }
       }
       ##### SandBox #####
       "${var.root_id}-sandboxes" = {
         display_name               = "${lower(var.root_id)}-sandboxes"
         parent_management_group_id = "${var.root_id}"
         subscription_ids           = []
-        archetype_config = {
-          # archetype_id   = "default_empty"
-          archetype_id   = "es_sandboxes"
-          parameters     = {}
-          access_control = {}
-        }
       }
     }
   depends_on = [
