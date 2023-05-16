0951ca489ab4c852e09f8cee5c0d8f86dc2f05d3
Ravinder Gupta <ragu@microsoft.com>
2023-05-16 17:32:33 +0530

    test

--- Modules/resourcegroup/main.tf
+++ Modules/resourcegroup/main.tf
@@ -5,8 +5,14 @@ resource "azurerm_resource_group" "this" {
   for_each = var.resource_groups
   name     = each.value["name"]
   location = each.value["location"]
+}
+
+resource "azurerm_resource_group_tag" "this" {
+  for_each      = azurerm_resource_group.this
+  resource_group_name = each.value.name
+
   tags = merge(
-    each.value["tags"],
+    each.value.tags,
     {
       date = formatdate("DD-MM-YYYY", timestamp())
     }
