8b7bcb4f6ac40103709293ce4256fbad1226f3f4
Ravinder Gupta <ragu@microsoft.com>
2023-05-29 20:07:35 +0530

    rearea

--- infra/modules/azurerm_resource_group/main.tf
+++ infra/modules/azurerm_resource_group/main.tf
@@ -20,9 +20,9 @@ resource "azurerm_virtual_network" "virtual_network" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_names
+  name                 = var.subnet_names[count.index]
   virtual_network_name = var.vnet1
   resource_group_name  = var.name
-  address_prefixes     = var.subnet_prefixes
+  address_prefixes     = [var.subnet_prefixes[count.index]]
   depends_on = [ azurerm_virtual_network.virtual_network ]
 }
