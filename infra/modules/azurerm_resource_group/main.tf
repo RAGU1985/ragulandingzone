f2a4d19160f0a3c6f9af4aa53a0300301fa0c486
Ravinder Gupta <ragu@microsoft.com>
2023-05-29 20:12:48 +0530

    fadfad

--- infra/modules/azurerm_resource_group/main.tf
+++ infra/modules/azurerm_resource_group/main.tf
@@ -19,10 +19,13 @@ resource "azurerm_virtual_network" "virtual_network" {
   depends_on = [ azurerm_resource_group.resource_group ]
 }
 
+locals {
+  subnets = zipmap(var.subnet_names, var.subnet_prefixes)
+}
 resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_names[count.index]
+  for_each             = local.subnets
+  name                 = each.key
   virtual_network_name = var.vnet1
-  resource_group_name  = var.name
-  address_prefixes     = [var.subnet_prefixes[count.index]]
+  address_prefixes     = each.value
   depends_on = [ azurerm_virtual_network.virtual_network ]
 }
