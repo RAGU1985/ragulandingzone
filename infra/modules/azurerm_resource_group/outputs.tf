6bad0a98aa4f1f310594d1037739220311e085fe
Ravinder Gupta <ragu@microsoft.com>
2023-05-29 15:09:25 +0530

    rereee55555555

--- infra/modules/azurerm_resource_group/outputs.tf
+++ infra/modules/azurerm_resource_group/outputs.tf
@@ -1,6 +1,6 @@
 # Output variables for Resource Group module
 
 output "resource_name" {
-  value       = azurerm_resource_group.resource_group.name
+  value = [for x in azurerm_resource_group.resource_group : x.id]
   description = "Output name of the created Resource Group"
 }
