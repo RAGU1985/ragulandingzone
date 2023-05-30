ad63e6c2cc9a37562dcd163b1469f76f0392aeea
Ravinder Gupta <ragu@microsoft.com>
2023-05-30 17:22:21 +0530

    tesert

--- infra/modules/azurerm_resource_group/main.tf
+++ infra/modules/azurerm_resource_group/main.tf
@@ -76,23 +76,14 @@ resource "azurerm_network_security_group" "network_security_group" {
     direction                    = "Inbound"
     access                       = "Allow"
     priority                     = 100
-    source_address_prefix        = "*"
-    source_port_range            = "*"
+    source_address_prefix        = "10.0.0.0/26"
+    source_address_prefixes      = null
     destination_address_prefix   = "*"
-    destination_port_range       = "3389"
-  }
-
-  security_rule {
-    name                         = "SSHInbound"
-    description                  = "NSG"
-    protocol                     = "Tcp"
-    direction                    = "Inbound"
-    access                       = "Allow"
-    priority                     = 101
-    source_address_prefix        = "*"
+    destination_address_prefixes = null
     source_port_range            = "*"
-    destination_address_prefix   = "*"
-    destination_port_range       = "22"
+    source_port_ranges           = null
+    destination_port_range       = null
+    destination_port_ranges      = ["3389", "22"]
   }
 }
 
