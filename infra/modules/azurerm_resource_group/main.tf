ec69ed37a66c34360f1e6412069d42d9b62cf5f7
Ravinder Gupta <ragu@microsoft.com>
2023-05-31 15:26:06 +0530

    fdafda

--- infra/modules/azurerm_resource_group/main.tf
+++ infra/modules/azurerm_resource_group/main.tf
@@ -70,30 +70,70 @@ resource "azurerm_network_security_group" "network_security_group" {
   resource_group_name = var.net_rg_name
 
   security_rule {
-    name                         = "Allow_RDP"
-    description                  = "Allow RDP traffic"
+    name                         = "AllowHTTPsInbound"
+    description                  = "AllowHTTPsInbound"
     protocol                     = "Tcp"
     direction                    = "Inbound"
     access                       = "Allow"
-    priority                     = 100
-    source_address_prefix        = "*"
+    priority                     = 120
+    source_address_prefix        = "Internet"
     destination_address_prefix   = "*"
     source_port_range            = "*"
-    destination_port_range       = "3389"
+    destination_port_range       = "443"
   }
 
   security_rule {
-    name                         = "Allow_SSH"
-    description                  = "Allow SSH traffic"
+    name                         = "AllowGatewayManagerInbound"
+    description                  = "AllowGatewayManagerInbound"
     protocol                     = "Tcp"
     direction                    = "Inbound"
     access                       = "Allow"
-    priority                     = 101
-    source_address_prefix        = "*"
+    priority                     = 130
+    source_address_prefix        = "GatewayManager"
     destination_address_prefix   = "*"
     source_port_range            = "*"
-    destination_port_range       = "22"
+    destination_port_range       = "443"
   }
+
+  security_rule {
+    name                         = "AllowAzureLoadBalancerInbound"
+    description                  = "AllowAzureLoadBalancerInbound"
+    protocol                     = "Tcp"
+    direction                    = "Inbound"
+    access                       = "Allow"
+    priority                     = 140
+    source_address_prefix        = "AzureLoadBalancer"
+    destination_address_prefix   = "*"
+    source_port_range            = "*"
+    destination_port_range       = "443"
+  }
+
+  security_rule {
+    name                         = "AllowBastionHostCommunication"
+    description                  = "AllowBastionHostCommunication"
+    protocol                     = "Tcp"
+    direction                    = "Inbound"
+    access                       = "Allow"
+    priority                     = 150
+    source_address_prefix        = "VirtualNetwork"
+    destination_address_prefix   = "*"
+    source_port_range            = "*"
+    destination_port_range       = ["8080", "5701"]
+  }
+
+  security_rule {
+    name                         = "SSHRDP"
+    description                  = "ssh rdp"
+    protocol                     = "Tcp"
+    direction                    = "Outbound"
+    access                       = "Allow"
+    priority                     = 100
+    source_address_prefix        = "*"
+    destination_address_prefix   = "VirtualNetwork"
+    source_port_range            = "*"
+    destination_port_range       = ["22", "3389"]
+  }
+
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsg-assoc" {
